9e5776734465950c64a9a539e83ccbd7feb72146
bar ivri <barevryaf@gmail.com>
2022-04-27 12:39:34 +0300

    first commit

---
+++ apps/keep/keep.jsx
@@ -0,0 +1,6 @@
+export class Keep extends React.Component{
+
+    render(){
+        return <h1>misssss keeeep</h1>
+    }
+}
---
+++ apps/mail/Mail.jsx
@@ -0,0 +1,8 @@
+export class Mail extends React.Component{
+    
+    render(){
+
+        return     <h1> welcome to mail home page</h1>
+    }
+
+}
