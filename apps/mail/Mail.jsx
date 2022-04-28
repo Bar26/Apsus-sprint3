9c1ebb7c19f86cc8ecaf5da03587845de485a587
bar ivri <barevryaf@gmail.com>
2022-04-28 16:17:11 +0300

    crud+filter

--- apps/mail/Mail.jsx
+++ apps/mail/Mail.jsx
@@ -1,19 +1,23 @@
 import { MailList } from "./cmps/mail-list.jsx"
 import { mailService } from "./mail-services/mail-service.js"
+import { MailHeader } from "./cmps/mail-header.jsx"
+import { FolderList } from "./cmps/folder-list.jsx"
 
 export class Mail extends React.Component {
 
     state = {
         mails: [],
         critiria: {
-            status: null,    //'inbox/sent/trash/draft'
+            status: 'inbox',    //'inbox/sent/trash/draft'
             txt: '',
             isRead: null, // (optional property, if missing: show all)
-            isStared: null, // (optional property, if missing: show all)
+            isStarred: false, // (optional property, if missing: show all)
             // lables: ['important', 'romantic'] // has any of the labels
 
         }
     }
+
+
 
     componentDidMount() {
         this.loadMails()
@@ -30,14 +34,72 @@ export class Mail extends React.Component {
     }
 
 
+
+    handleChange = ({ target }) => {
+        const value = target.value
+        console.log(value)
+        const field = target.name
+        console.log(field)
+        this.setState((prevState) => ({ critiria: { ...prevState.critiria, [field]: value } }), () => this.loadMails())
+
+    }
+
+    onUpdateReadState = (mail) => {
+        console.log(mail)
+        mailService.updateReadState(mail)
+        this.loadMails()
+    }
+
+    onDelete = (mail) => {
+        mailService.deleteMail(mail)
+        this.loadMails()
+
+    }
+
+    onUpdateStarredState = (mail) => {
+        mailService.updateStarredState(mail)
+        this.loadMails()
+        console.log(this.inputRef.current.classList)
+
+    }
+
+    onUpdateCritiriaStatus = (status) => {
+        this.setState((prevState) => ({ critiria: { ...prevState.critiria, status } }), () => this.loadMails())
+
+
+    }
+
+    componentDidUpdate() {
+        console.log(this.state)
+    }
+
+    onUpdateCritiriaBooolian = (boolian) => {
+
+        this.setState((prevState) => ({ critiria: { ...prevState.critiria, [boolian]: !this.state.critiria[boolian] } }), () => this.loadMails())
+    }
+
+    onCompose = () => {
+        
+    }
+
+
+
+
     render() {
         const { mails } = this.state
-        console.log(mails)
-
-
         return <section>
-            <h1> welcome to mail home page</h1>
-            <MailList mails={mails} />
+            <MailHeader handleChange={this.handleChange} />
+            <section className="main-content flex clean-list">
+                <FolderList onCompose={this.onCompose} onUpdateCritiriaStatus={this.onUpdateCritiriaStatus} onUpdateCritiriaBooolian={this.onUpdateCritiriaBooolian} />
+                <MailList onDelete={this.onDelete} onUpdateStarredState={this.onUpdateStarredState} mails={mails} onUpdateReadState={this.onUpdateReadState} />
+
+            </section>
+            <section>
+                {/* <Switch>
+                <Route path="/about/team" component={Team} />
+                <Route path="/about/vision" component={Vision} />
+            </Switch> */}
+            </section>
         </section>
     }
 
--- apps/mail/mail-services/mail-service.js
+++ apps/mail/mail-services/mail-service.js
@@ -2,7 +2,10 @@ import { storageService } from "../../../services/storage.service.js"
 import { utilService } from "../../../services/util.service.js"
 
 export const mailService = {
-    query
+    query,
+    updateReadState,
+    updateStarredState,
+    deleteMail
 }
 
 
@@ -13,78 +16,117 @@ const loggedinUser = {
     fullname: 'Mahatma Appsus'
 }
 
-const gMails=[
+const gMails = [
     {
         id: 'e101',
         subject: 'Miss you!',
-        body: 'Would love to catch up sometimes',
+        body: 'Would love to catch up sometimesipsum dolor sit amet consectetur adipisicing elit. Voluptates magnam porro, dolorum est error fuga itaque minima veniam consectetur incidunt, doloremque dolorem! Qui culpa vitae voluptatem facilis et placeat praesentium',
         isRead: false,
+        isStarred: false,
         sentAt: Date.now(),
-        from:'Rachel Green',
+        from: 'Rachel Green',
         to: 'user@appsus.com',
+        status: 'inbox'
     },
     {
         id: 'e102',
         subject: 'how you doinggg!',
-        body: 'Would love to catch up sometimes',
+        body: 'Would love to catch up sometimesipsum dolor sit amet consectetur adipisicing elit. Voluptates magnam porro, dolorum est error fuga itaque minima veniam consectetur incidunt, doloremque dolorem! Qui culpa vitae voluptatem facilis et placeat praesentium',
         isRead: false,
+        isStarred: false,
         sentAt: Date.now(),
-        from:'joey tribbiani',
+        from: 'joey tribbiani',
         to: 'user@appsus.com',
+        status: 'inbox'
     },
     {
         id: 'e103',
         subject: 'Miss you!',
-        body: 'Would love to catch up sometimes',
-        isRead: false,
+        body: 'Would love to catch up sometimesipsum dolor sit amet consectetur adipisicing elit. Voluptates magnam porro, dolorum est error fuga itaque minima veniam consectetur incidunt, doloremque dolorem! Qui culpa vitae voluptatem facilis et placeat praesentium',
+        isRead: true,
+        isStarred: false,
         sentAt: Date.now(),
-        from:'Monika Geler',
+        from: 'Monica Geler',
         to: 'user@appsus.com',
+        status: 'inbox'
     },
     {
         id: 'e104',
         subject: 'lets push and commit!',
-        body: 'Would love to catch up sometimes',
+        body: 'Would love to catch up sometimesipsum dolor sit amet consectetur adipisicing elit. Voluptates magnam porro, dolorum est error fuga itaque minima veniam consectetur incidunt, doloremque dolorem! Qui culpa vitae voluptatem facilis et placeat praesentium',
         isRead: false,
+        isStarred: false,
         sentAt: Date.now(),
-        from:'Ross Geller',
+        from: 'Ross Geller',
         to: 'user@appsus.com',
+        status: 'inbox'
     },
     {
         id: 'e105',
         subject: 'Sale- only today!',
-        body: 'Would love to catch up sometimes',
-        isRead: false,
-        sentAt: Date.now(),
-        from:'Chendler Bing',
+        body: 'Would love to catch up sometimesipsum dolor sit amet consectetur adipisicing elit. Voluptates magnam porro, dolorum est error fuga itaque minima veniam consectetur incidunt, doloremque dolorem! Qui culpa vitae voluptatem facilis et placeat praesentium ',
+        isRead: true,
+        isStarred: false,
+        sentAt: 1251132930294,
+        from: 'Chendler Bing',
         to: 'user@appsus.com',
+        status: 'inbox'
     },
     {
         id: 'e106',
         subject: 'Miss you!',
-        body: 'Would love to catch up sometimes',
+        body: 'Would love to catch up sometimeipsum dolor sit amet consectetur adipisicing elit. Voluptates magnam porro, dolorum est error fuga itaque minima veniam consectetur incidunt, doloremque dolorem! Qui culpa vitae voluptatem facilis et placeat praesentium',
         isRead: false,
-        sentAt: Date.now(),
-        from:'pheobe buffay',
+        isStarred: false,
+        sentAt: 1551133930594,
+        from: 'pheobe buffay',
         to: 'user@appsus.com',
+        status: 'sent'
     }
 ]
 
 const email = {
     id: 'e101',
     subject: 'Miss you!',
-    body: 'Would love to catch up sometimes',
-    isRead: false,
+    body: 'Would love to catch up sometimes Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates magnam porro, dolorum est error fuga itaque minima veniam consectetur incidunt, doloremque dolorem! Qui culpa vitae voluptatem facilis et placeat praesentium',
+    isRead: true,
     sentAt: 1551133930594,
-    from:'user@appsus.com',
+    from: 'user@appsus.com',
     to: 'momo@momo.com',
+    status: 'sent'
 }
+
+function updateReadState(mailToUpdate) {
+    let mails = storageService.loadFromStorage(MAIL_KEY)
+    mailToUpdate.isRead = !mailToUpdate.isRead
+    mails = mails.map(mail => mail.id === mailToUpdate.id ? mailToUpdate : mail)
+    storageService.saveToStorage(MAIL_KEY, mails)
+}
+
 
 function query(critirea) {
     let mails
+    console.log(critirea)
     mails = storageService.loadFromStorage(MAIL_KEY)
-    if(!mails||!mails.length) mails=gMails
-    if(!critirea.status||!critirea.txt||!critirea.isRead||!critirea.isStared) return Promise.resolve(mails)
+    if (!mails || !mails.length) {
+        mails = gMails
+        storageService.saveToStorage(MAIL_KEY, mails)
+    }
+
+    // if(!critirea.status||!critirea.txt||!critirea.isRead||!critirea.isStared) return Promise.resolve(mails)
+    mails = mails.filter(mail => mail.status === critirea.status && (mail.body.includes(critirea.txt) ||
+        mail.subject.includes(critirea.txt) || mail.from.includes(critirea.txt)))
+    if (critirea.isRead !== null && critirea.isRead !== 'all') {
+        console.log(critirea.isRead)
+        console.log(critirea.isRead)
+        mails = mails.filter(mail => {
+
+            return mail.isRead + '' === critirea.isRead
+        })
+    }
+    if (critirea.isStarred) mails = mails.filter(mail => mail.isStarred)
+    console.log(mails)
+    return Promise.resolve(mails)
 }
 
 function _createMail(subject, body, to) {
@@ -98,3 +140,20 @@ function _createMail(subject, body, to) {
         to
     }
 }
+
+function updateStarredState(mailToUpdate) {
+    let mails = storageService.loadFromStorage(MAIL_KEY)
+    mailToUpdate.isStarred = !mailToUpdate.isStarred
+    mails = mails.map(mail => mail.id === mailToUpdate.id ? mailToUpdate : mail)
+    storageService.saveToStorage(MAIL_KEY, mails)
+}
+
+function deleteMail(mailToDelete) {
+    console.log(mailToDelete)
+    let mails = storageService.loadFromStorage(MAIL_KEY)
+    const idx = mails.findIndex(mail => mail.id === mailToDelete.id)
+    console.log(idx)
+    mails.splice(idx,1)
+    storageService.saveToStorage(MAIL_KEY, mails)
+}
+ 
